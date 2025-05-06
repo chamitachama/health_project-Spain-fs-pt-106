41306590d1cbed6024222a454a26a0cb7fc36bcc
Gen <genhatesales@gmail.com>
2025-05-06 19:00:29 +0200

    Update to original  main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+// index.css'
+import "../styles/index.css";
+import "../styles/calendar.css";
+
+// components
+// import Home from "./components/Home";
+import Sidebar from "./components/Sidebar";
+import Layout from "./components/Layout";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Layout></Layout>
+  </React.StrictMode>,
 );
