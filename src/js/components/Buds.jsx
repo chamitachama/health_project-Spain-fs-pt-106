0c3e19cea344251d22d16cc6d76ef9ad9b1de98a
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2025-05-08 12:55:47 +0000

    FONDO COMP 2 ALFIN LISTO QUE MILAGRO

--- src/js/components/Buds.jsx
+++ src/js/components/Buds.jsx
@@ -5,26 +5,19 @@ import success from "../../img/success.png";
 function Buds() {
   return (
     <>
-      <div className="outer-div">
-        <div className="card mb-3" style={{ maxWidth: "200px" }}>
-          <div className="row g-0">
-            <div className="pill col-md-4">
-              <img src={cross} className="img-fluid rounded-start" alt="X" />
+      <div className="lungs-outer-div border-0 rounded-5 d-flex flex-column justify-content-center h-50 shadow">
+          <div className="d-flex flex-row m-2">
+          <div className="col flex-grow-5">
+              <small className="text-muted">Kidneys</small>
+              <h5 className="card-title">Hurt</h5>
             </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <small className="text-muted">Buds</small>
-                <p className="card-text"></p>
-                <p className="card-text">
-                  <h5 className="card-title">Hurted</h5>
-                </p>
-              </div>
+            <div className="col-3 ">
+              <img src="https://cdn1.iconfinder.com/data/icons/smallicons-controls/32/614397-x-512.png" className="img-fluid rounded-start" alt="X" />
             </div>
           </div>
-        </div>
       </div>
     </>
   );
-}
+};
 
 export default Buds;
